0a998e435f9a1689e084e5737d3200a32e4d8db1
Victor Hugo Silva <90207063+VictorHugoSDev@users.noreply.github.com>
2024-10-11 15:15:56 -0300

    Update script11.js

--- js/script11.js
+++ js/script11.js
@@ -7,9 +7,15 @@ while(cartaUnica.size < 12){
 
 var cartasSorteadas = Array.from(cartaUnica);
 
+document.write(`<h1 style="text-align: center;">JOGUE TRUCO</h1>`)
+document.write(`<h4 style="color: red; text-align: center;">Cartas Sorteadas</h4>`)
+
 for(var i = 0; i < 4; i++){
-    document.write(`<h1>CARTA SORTEADA JOGADOR ${i + 1}</h1>`)
+    document.write(`<h2 style="text-align: center;">Cartas do Jogador ${i + 1}:</h2>`)
+    
+    document.write(`<div style="text-align: center;">`);
     for(var j = 0; j < 3; j++){
-        document.write(`<img src="cartas/carta${cartasSorteadas[i * 3 + j]}.png">`);
+        document.write(`<img src="cartas/carta${cartasSorteadas[i * 3 + j]}.png" style="margin: 10px;">`);
     }
+    document.write(`</div>`);
 }
